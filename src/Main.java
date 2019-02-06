9b4461022675a84f24a43ea7c3047e0e5539d26c
calin <calin.cioban@gmail.com>
2019-02-06 20:30:02 +0200

    qw

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args);
+
+
+    int adunare = 2 + 3;
+   ''[system.out.println(adunare);
+    adunare++;
+    adunare +=3;
+
+
+    int scadere = 2 - 45000;
+
+    {
         System.out.println("Hello World!");
     }
 }
+[]private static final
